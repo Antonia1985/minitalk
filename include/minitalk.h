f9e467c753095a087bdb73511a1061d41988ce21
Antonia <33874536+Antonia1985@users.noreply.github.com>
2025-01-30 20:44:21 +0100

    Update minitalk.h

--- include/minitalk.h
+++ include/minitalk.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apavlopo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 19:28:23 by apavlopo          #+#    #+#             */
+/*   Updated: 2025/01/30 19:28:48 by apavlopo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 #define MINITALK_H
 
@@ -5,10 +17,10 @@
 #include <signal.h>
 #include <sys/types.h>
 
-int main(int ac, char **av);
+int     main(int ac, char **av);
 void encode_and_send_bits(char ch, pid_t servers_pid);
 
-int main();
+int     main();
 void handle_signal (int signum);
 
 #endif

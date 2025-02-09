b7d8d1ab313f4ffc6407d3604db2ad96aec7ef69
Antonia <33874536+Antonia1985@users.noreply.github.com>
2025-02-09 19:55:01 +0100

    Update server.c 9/02

--- src/server.c
+++ src/server.c
@@ -10,48 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void handle_signal (int signum, siginfo_t *info, void *context)
+#include "libft.h"
+#include "minitalk.h"
+
+void	handle_signal(int signum, siginfo_t *info, void *context)
 {
-    static int bit_counter = 0;
-    static char ch = 0;
-
-    (void)info;    // Prevent unused warning
-    (void)context; // Prevent unused warning
-
-    
-    ch = ch << 1;
-    if (signum == SIGUSR2)
-    ch |= 1;
-    
-    bit_counter++;
-    if (bit_counter == 8)
-    {
-        write(1, &ch, 1);
-        bit_counter = 0;
-        ch = 0;
-    }
-        
+	static int	bit_counter  = 0;
+	static char	ch = 0;
+	pid_t	client_pid= info -> si_pid;
+	
+	(void)info;
+	(void)context;
+	ch = ch << 1;
+	if (signum == SIGUSR2)
+		ch |= 1;
+	bit_counter++;
+	if (bit_counter == 8)
+	{
+		if (ch == '\0')
+		{
+			kill(client_pid, SIGUSR1);
+		}
+		write(1, &ch, 1);
+		bit_counter = 0;
+		ch = 0;
+	}
 }
 
-int main()
+int	main(void)
 {
-    pid_t servers_pid = getpid();
-    ft_printf("PID: %d\n", servers_pid);
+	pid_t	servers_pid;
+	struct sigaction	sa;
 
-    struct sigaction sa;    
-    sa.sa_sigaction = handle_signal;
-    sa.sa_flags = SA_SIGINFO;
-    sigemptyset(&sa.sa_mask);
-
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-
-    while (1)
-    {
-        //wait for signals from client
-        pause();
-
-    }
-
-    return (0);    
+	servers_pid = getpid();
+	ft_printf("PID: %d\n", servers_pid);
+	sa.sa_sigaction = handle_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+	{
+		pause();
+	}
+	return (0);
 }
